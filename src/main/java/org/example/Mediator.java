4485f1acd0827b973a418ffd1e0cecce2be5b84d
KaMuench <muench.kaleb@gmail.com>
2022-11-07 14:46:41 +0100

    mediator constructor

--- src/main/java/org/example/Mediator.java
+++ src/main/java/org/example/Mediator.java
@@ -1,4 +1,16 @@
 package org.example;
 
+
 public class Mediator {
+    Supplier mSupplier;
+    Customer mCustomer;
+    int mJobsAmount;
+
+    Mediator(Supplier supplier, Customer customer, int jobsAmount) {
+        mCustomer = customer;
+        mSupplier = supplier;
+        mJobsAmount = jobsAmount;
+    }
+
+
 }
