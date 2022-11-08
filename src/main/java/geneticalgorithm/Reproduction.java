2ccf9b39f72e4039c90ef29e5ac9586d19f4bbae
Simeon Wagner <wagnersimeon01@gmail.com>
2022-11-08 10:33:20 +0100

    - genetic algorithm

--- src/main/java/geneticalgorithm/Reproduction.java
+++ src/main/java/geneticalgorithm/Reproduction.java
@@ -1,6 +1,9 @@
 package geneticalgorithm;
 
 public class Reproduction {
-
+    public static Individual [] one_point_crossover (Individual father, Individual mother){
+        //your code
+        return null;
+    }
 
 }
