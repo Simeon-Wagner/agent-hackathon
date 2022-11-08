90d426420a2ccd0113ff35c9fd4c8a77db7c5cd5
luuiisee <116493538+luuiisee@users.noreply.github.com>
2022-11-08 12:21:35 +0100

    supplier komplexere vote-Methode

--- src/main/java/org/example/Supplier.java
+++ src/main/java/org/example/Supplier.java
@@ -3,13 +3,24 @@ package org.example;
 public class Supplier {
 
 
+    public Supplier(int[][] costMatrix, int rounds, double mustAccept) {
+        this.costMatrix = costMatrix;
+        this.rounds = rounds;
+        this.mustAccept = mustAccept;
+        this.roundNr = 0;
+        this.roundsAccepted = 0;
+    }
     public Supplier(int[][] costMatrix) {
         this.costMatrix = costMatrix;
     }
     public void setCostMatrix(int[][] costMatrix) {
         this.costMatrix = costMatrix ;
     }
-    private int [][]costMatrix = {{0, 6, 8, 2}, {4, 0, 2, 3},{3, 7, 0, 6},{2, 8, 4, 0}};
+    private int [][]costMatrix;
+    int rounds;
+    int roundNr;
+    int roundsAccepted;
+    double mustAccept;
     public  int getTime(int [] proposal) {
         int time = 0;
         for(int i = 0; i< proposal.length-1; i++){
@@ -17,7 +28,28 @@ public class Supplier {
         }
         return time;
     }
-    public boolean vote (int [] contract, int[] proposal){
-        return getTime(proposal) < getTime(contract);
+    public boolean vote(int [] contract, int[] proposal){
+        roundNr++;
+        int timeP = getTime(proposal);
+        int timeC = getTime(contract);
+        double relDif = (timeP-timeC)/timeC;
+        if(timeP<timeC) {
+            roundsAccepted++;
+            return true;
+        } else if (getAcceptanceRate()<mustAccept&&(relDif<0.1)) {
+            roundsAccepted++;
+            return true;
+        }else if((rounds-roundNr)==(rounds*mustAccept-roundsAccepted)) {
+            roundsAccepted++;
+            return true;
+        }else {
+            return false;
+        }
     }
+    double getAcceptanceRate (){
+        return roundsAccepted/roundNr;
+    }
+  /*  public boolean vote (int [] contract, int[] proposal){
+        return getTime(proposal) < getTime(contract);
+    }*/
 }
